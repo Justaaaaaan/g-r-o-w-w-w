d733c003f6e755f8630d03653b92fe8705fe0cdf
Justaaaaaan <78387349+Justaaaaaan@users.noreply.github.com>
2021-03-04 15:08:52 -0500

    MORE CHANGES

---
+++ resize-and-drag.js
@@ -0,0 +1,12 @@
+$( ".resizable" ).resizable({
+    handles:"n,e,s,w"
+    // helper: "resizable-helper"
+    // containment: "parent"
+    // animate: true
+    // aspectRatio: true,
+    // grid: [ 30,30 ] 
+});
+$( ".draggable").draggable({
+
+
+});
